25b2f23f7cd75e14bf2314571c47b2b18af1d08a
Anurag Dangi <anuragdangi015@gmail.com>
2026-03-11 12:01:30 +0530

    feat(chess): connected to production backend

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,10 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+// const socket = io('http://localhost:5000', {
+//   autoConnect: false,
+// });
+
+const socket = io('https://chess-backend-xdgj.onrender.com', {
   autoConnect: false,
 });
 
